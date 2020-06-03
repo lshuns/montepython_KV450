53f1db2206bf8fcb7a11ee3695c5deb7c8b65be2
S L <1006682997@qq.com>
2020-06-03 11:04:28 +0200

    data vectors

---
+++ data/KV450_less3/redshift/Nz_DIR/Nz_DIR_Mean/rename.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+in_prefix=KiDS_2018-07-26_deepspecz_photoz_10th_BLIND_specweight_1000_4
+in_suffix=TBle3_blindB_Nz.asc
+in_names=("ZB0p1t0p3" "ZB0p3t0p5" "ZB0p5t0p7" "ZB0p7t0p9" "ZB0p9t1p2")
+
+
+out_prefix=Nz_DIR
+out_suffix=.asc
+out_names=("z0.1t0.3" "z0.3t0.5" "z0.5t0.7" "z0.7t0.9" "z0.9t1.2")
+
+
+for i in ${!in_names[@]};
+do
+    in_name=${in_prefix}_${in_names[$i]}_${in_suffix}
+    out_name=${out_prefix}_${out_names[$i]}${out_suffix}
+    cp $in_name $out_name
+done
